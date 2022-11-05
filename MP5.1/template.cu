106ea1acf57bff0cf4ac4a5da1508992d9cb9f73
Cheng-Han <72143567+johnchiang106@users.noreply.github.com>
2022-11-04 21:43:00 -0500

    MP5.1

---
+++ MP5.1/template.cu
@@ -0,0 +1,129 @@
+// MP 5.1 Reduction
+// Given a list of length n
+// Output its sum = lst[0] + lst[1] + ... + lst[n-1];
+
+#include <wb.h>
+
+#define BLOCK_SIZE 512 //@@ You can change this
+#define TIMES 2
+
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
+      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
+      return -1;                                                          \
+    }                                                                     \
+  } while (0)
+  
+__global__ void total(float *input, float *output, int len) {
+  //@@ Load a segment of the input vector into shared memory
+  //@@ Traverse the reduction tree
+  //@@ Write the computed sum of the block to the output vector at the
+  //@@ correct index
+  __shared__ float partialSum[BLOCK_SIZE * TIMES];
+  int dx = blockDim.x, bx = blockIdx.x, tx = threadIdx.x;
+  unsigned int start = TIMES * bx * dx + tx;
+  if(start < len)
+    partialSum[tx] = input[start];
+  else
+    partialSum[tx] = 0;
+  if(start + dx < len)
+    partialSum[tx+dx] = input[start + dx];
+  else
+    partialSum[tx+dx] = 0;
+  for (unsigned int stride = dx; stride >= 1; stride /= 2) {
+    __syncthreads();
+    if (tx < stride)
+      partialSum[tx] += partialSum[tx + stride];
+  }
+  // output[bx*dx+tx] = partialSum[tx];
+  output[bx] = partialSum[0];
+}
+
+int main(int argc, char **argv) {
+  int ii;
+  wbArg_t args;
+  float *hostInput;  // The input 1D list
+  float *hostOutput; // The output list
+  float *deviceInput;
+  float *deviceOutput;
+  int numInputElements;  // number of elements in the input list
+  int numOutputElements; // number of elements in the output list
+
+  args = wbArg_read(argc, argv);
+
+  wbTime_start(Generic, "Importing data and creating memory on host");
+  hostInput =
+      (float *)wbImport(wbArg_getInputFile(args, 0), &numInputElements);
+
+  // numOutputElements = numInputElements / (BLOCK_SIZE * 2);
+  // if (numInputElements % (BLOCK_SIZE * 2)) numOutputElements++;
+  numOutputElements = (numInputElements - 1) / (BLOCK_SIZE << 1) + 1;
+  // numOutputElements = (numInputElements - 1) / (BLOCK_SIZE * TIMES) + 1;
+  // numOutputElements *= BLOCK_SIZE;
+  hostOutput = (float *)malloc(numOutputElements * sizeof(float));
+
+  wbTime_stop(Generic, "Importing data and creating memory on host");
+
+  wbLog(TRACE, "The number of input elements in the input is ",
+        numInputElements);
+  wbLog(TRACE, "The number of output elements in the input is ",
+        numOutputElements);
+
+  wbTime_start(GPU, "Allocating GPU memory.");
+  //@@ Allocate GPU memory here
+  cudaMalloc((void **) &deviceInput, numInputElements * sizeof(float));
+  cudaMalloc((void **) &deviceOutput, numOutputElements * sizeof(float));
+
+  wbTime_stop(GPU, "Allocating GPU memory.");
+
+  wbTime_start(GPU, "Copying input memory to the GPU.");
+  //@@ Copy memory to the GPU here
+  cudaMemcpy(deviceInput, hostInput, numInputElements * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceOutput, hostOutput, numOutputElements * sizeof(float), cudaMemcpyHostToDevice);
+
+  wbTime_stop(GPU, "Copying input memory to the GPU.");
+  //@@ Initialize the grid and block dimensions here
+  dim3 DimGrid(numInputElements/BLOCK_SIZE,1,1);
+  if (numInputElements%BLOCK_SIZE) DimGrid.x++;
+  dim3 DimBlock(BLOCK_SIZE,1,1);
+
+
+  wbTime_start(Compute, "Performing CUDA computation");
+  //@@ Launch the GPU Kernel here
+  total<<<DimGrid,DimBlock>>>(deviceInput, deviceOutput, numInputElements);
+
+  cudaDeviceSynchronize();
+  wbTime_stop(Compute, "Performing CUDA computation");
+
+  wbTime_start(Copy, "Copying output memory to the CPU");
+  //@@ Copy the GPU memory back to the CPU here
+  cudaMemcpy(hostOutput, deviceOutput, numOutputElements * sizeof(float), cudaMemcpyDeviceToHost);
+
+  wbTime_stop(Copy, "Copying output memory to the CPU");
+
+  /***********************************************************************
+   * Reduce output vector on the host
+   * NOTE: One could also perform the reduction of the output vector
+   * recursively and support any size input.
+   * For simplicity, we do not require that for this lab!
+   ***********************************************************************/
+  for (ii = 1; ii < numOutputElements; ii++) {
+    hostOutput[0] += hostOutput[ii];
+  }
+
+  wbTime_start(GPU, "Freeing GPU Memory");
+  //@@ Free the GPU memory here
+  cudaFree(deviceInput); cudaFree (deviceOutput);
+
+  wbTime_stop(GPU, "Freeing GPU Memory");
+
+  wbSolution(args, hostOutput, 1);
+
+  free(hostInput);
+  free(hostOutput);
+
+  return 0;
+}
